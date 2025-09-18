203642741fc1dfe1b91975fe26cf0dcf89b79d63
RENJITHRAJ-RS <renjithraj911@gmail.com.com>
2025-09-18 19:54:43 +0530

    sql

---
+++ demo.sql
@@ -0,0 +1,4 @@
+INSERT INTO Customers (CustomerName, City, PostalCode, Country)
+VALUES ('Ali','Dubai','58063','UAE'), 
+       ('Akshay','Kochi','695311','India'),
+       ('Chan','Xining','S-896 23','China');
