82589f618a55355ba5c674fcc9c76f781b487c6b
RENJITHRAJ-RS <renjithraj911@gmail.com.com>
2025-09-22 21:17:17 +0530

    Add empty SQL script for training center

---
+++ Training center.sql
@@ -0,0 +1,4 @@
+INSERT INTO `training center`(`id`, `name`, `course`, `fees paid`, `status`) VALUES
+(1, 'Alice', 'Web Development', 5000, 'Inactive'),
+(2, 'Bob', 'Data Science', 7000, 'Inactive'),
+(3, 'Charlie', 'UI/UX Design', 4000, 'Active')
