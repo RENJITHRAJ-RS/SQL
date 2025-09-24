70eea81296429c49d1d8310c3cb3c9bae62a1e10
RENJITHRAJ-RS <renjithraj911@gmail.com.com>
2025-09-24 12:36:39 +0530

    Add empty SQL scripts for book and mobile store

---
+++ Book.sql
@@ -0,0 +1,4 @@
+INSERT INTO `book`(`id`, `title`, `author`, `genre`, `price`, `copies_sold`) VALUES 
+(1, 'The Silent Patient', 'Alex Michaelides', 'Thriller', 399.00, 1200),
+(2, 'Atomic Habits', 'James Clear', 'Self-help', 499.00, 2000),
+(3, 'The Psychology of Money', 'Morgan Housel', 'Finance', 350.00, 1800)
